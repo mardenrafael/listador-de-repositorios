96475cec4ca28ca6a75e6562cf807d3a74fd5526
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-04-01 15:30:28 -0400

    Styles completed

---
+++ src/components/Display.js
@@ -0,0 +1,106 @@
+import styled from 'styled-components';
+import SearchIcon from '../img/Search-icon.svg';
+import { Component } from 'react';
+
+
+const DisplayBody = styled.div`
+    width: 70%;
+    height: 380px;
+
+    
+    margin: auto;
+    margin-top: 50px;
+
+    box-shadow: 0 0 10px 10px rgba(0, 0, 0, 0.5);
+    border-radius: 30px;
+
+    padding: 30px;
+`;
+
+const InputDiv = styled.div`
+    width: 30%;
+    height: 10%;
+
+    display: flex;
+
+    align-items: center;
+
+    background-color: #f7f2ff;
+
+    border-radius: 30px;
+
+    box-shadow: 0 0 10px 10px rgba(0, 0, 0, 0.5);
+
+    padding: 0px 30px 0 10px;
+`;
+
+const RepoInput = styled.input`
+    font-weight: 400;
+    font-size: large;
+    color: #341269;
+
+    background-color: #f7f2ff;
+
+    border: none;
+
+    width: 100%;
+    height: 100%;
+
+    padding: 0;
+
+    margin-left: 2px;
+    
+    :focus {
+        outline: none;
+    }
+`;
+
+const SearchIconImg = styled.img`
+    height: 90%;
+    width: 70px;
+`;
+
+const DisplayList = styled.div`
+    
+    border: 2px solid rgb(11, 2, 26);
+    border-radius: 30px;
+
+    padding: 30px;
+
+    margin-top: 50px;
+    
+    width: 93.5%;
+    height: 225px;
+
+`;
+
+const List = styled.ul`
+    padding: 0;
+    margin: 0;
+
+    list-style: none;
+`;
+
+class Display extends Component {
+
+    render() {
+        return(
+            <DisplayBody>
+                <InputDiv>
+                    <SearchIconImg alt='Pesquisar' src={SearchIcon}></SearchIconImg>
+                    <RepoInput placeholder='username/user-repo'></RepoInput>
+                </InputDiv>
+                
+                <DisplayList>
+                    <List>
+                        <li>
+                            <a href='#'>Link</a>
+                        </li>
+                    </List>
+                </DisplayList>
+            </DisplayBody>
+        );
+    }
+}
+
+export default Display;
