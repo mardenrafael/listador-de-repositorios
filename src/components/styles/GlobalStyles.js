bd013210617b92a8c34fb30d21490a90774d66db
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-04-01 14:00:31 -0400

    create GlobalStyle

---
+++ src/components/styles/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: #F7F2FF;
+
+        padding: 0;
+        margin: 0;
+    }
+`;
+
+export default GlobalStyle;
